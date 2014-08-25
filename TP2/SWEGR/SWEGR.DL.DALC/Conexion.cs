1a44e50f3c92fde18d4559f537b96e4fb861724c
Giancarlo Renzo <giannmakoto@gmail.com>
2014-08-25 17:32:27 +1200

    Creación del proyecto

---
+++ TP2/SWEGR/SWEGR.DL.DALC/Conexion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace SWEGR.DL.DALC
+{
+    class Conexion
+    {
+        private SqlConnection sqlconnection;
+
+        public SqlConnection obtenerConexion()
+        {
+            string cadena = ConfigurationManager.ConnectionStrings["ConexionDB"].ConnectionString;
+            sqlconnection = new SqlConnection(cadena);
+ 
+            return sqlconnection;
+        }
+
+    }
+}
---
+++ TP2/SWEGR/SWEGR.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SWEGR.UI.Global" Language="C#" %>
